50032794905251201ad56a48677c53cb6658ade4
alphacustomsnj-oss <alphacustomsnj@gmail.com>
2026-07-19 02:57:49 -0400

    Add files via upload

--- packing/sw.js
+++ packing/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'co-packing-v17';
+const CACHE = 'co-packing-v18';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html'])).then(()=>self.skipWaiting()));
 });

7e3decc6f50d3797969cabe93cff68c54d2461e8
alphacustomsnj-oss <alphacustomsnj@gmail.com>
2026-07-22 18:10:00 -0400

    Add files via upload

--- auction/sw.js
+++ auction/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'co-auction-v56';
+const CACHE = 'co-auction-v59';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./','./index.html'])).then(()=>self.skipWaiting()));
 });
